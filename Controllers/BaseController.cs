950f77599b9deabb2e44c71327c1ae6166bec460
AffectedArc07 <25063394+AffectedArc07@users.noreply.github.com>
2021-08-10 12:42:59 +0100

    WIP Server Stats Page

--- Controllers/BaseController.cs
+++ Controllers/BaseController.cs
@@ -1,11 +1,14 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
 using SS13Stats.Frontend.Database;
+using SS13Stats.Frontend.Helpers;
 using SS13Stats.Frontend.Models;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace SS13Stats.Frontend.Controllers {
@@ -33,9 +36,29 @@ namespace SS13Stats.Frontend.Controllers {
             return View("Index", slm);
         }
 
-        [Route("server/{serverid}")]
-        public IActionResult GetServer(int serverid) {
-            return Ok("TOOD");
+        [Route("server/{serverid?}")]
+        public IActionResult GetServer(int? serverid) {
+            if(serverid == null) {
+                return BadRequest("Please supply a server ID");
+            }
+            ServerViewModel svm = new ServerViewModel();
+            if(_dbc.Servers.Where(s => s.Id == serverid).Any()) {
+                svm.server = _dbc.Servers.Where(s => s.Id == serverid).First();
+            } else {
+                return NotFound("A server with ID " + serverid + " could not be found");
+            }
+
+            JsonSerializerSettings settings = new JsonSerializerSettings();
+            settings.ContractResolver = new DictionaryAsArrayResolver();
+
+            Dictionary<DateTime, int> snaps_24h = _dbc.Snapshots.Where(s => s.Server == svm.server).Where(s => s.SnapshotTime > DateTime.Now.AddDays(-1)).OrderBy(s => s.SnapshotTime).ToDictionary(s => s.SnapshotTime, s => s.PlayerCount);
+            
+            svm.day_snapshots_json = JsonConvert.SerializeObject(snaps_24h, settings);
+
+            Dictionary<DateTime, int> snaps_7d = _dbc.Snapshots.Where(s => s.Server == svm.server).Where(s => s.SnapshotTime > DateTime.Now.AddDays(-7)).OrderBy(s => s.SnapshotTime).ToDictionary(s => s.SnapshotTime, s => s.PlayerCount);
+            svm.week_snapshots_json = JsonConvert.SerializeObject(snaps_7d, settings);
+
+            return View("ServerView", svm);
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
---
+++ Models/ServerViewModel.cs
@@ -0,0 +1,13 @@
+﻿using SS13Stats.Frontend.Database;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SS13Stats.Frontend.Models {
+    public class ServerViewModel {
+        public Server server { get; set; }
+        public string day_snapshots_json { get; set; }
+        public string week_snapshots_json { get; set; }
+    }
+}
